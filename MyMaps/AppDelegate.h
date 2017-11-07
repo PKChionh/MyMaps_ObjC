eff5cb18e7fb67883d3a4f2dd673a44749569721
ITSAdmin <ITSAdmin@cw4305mac41.local>
2017-11-07 09:21:56 +0800

    Initial Commit

---
+++ MyMaps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMaps
+//
+//  Created by ITSAdmin on 7/11/17.
+//  Copyright © 2017 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMaps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMaps
+//
+//  Created by ITSAdmin on 7/11/17.
+//  Copyright © 2017 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
